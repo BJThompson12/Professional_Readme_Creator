db65c0b219e33f4670cf9f4f272b1c0d055914f8
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-02-22 21:57:54 -0500

    set up git ignore and inquirer install

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +14,26 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your languages do you know?',
+      name: 'language',
+    },
+    {
+      type: 'input',
+      message: 'What is your preferred method of communication?',
+      name: 'method',
+    },
+  ])
+  .then((response) =>
+    console.log(response.username, response.language, response.method)
+  );
