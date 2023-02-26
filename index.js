e4a08782220a31523c0f61d1d87fe5c26b25e209
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-02-26 18:57:23 -0500

    start questions array and add needed varioables

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
+const generateReadMe = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
-
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Provide a short description explaining the whatm why and how of your porject.',
+    name: 'descriptin',
+  },
 ];
 
 // TODO: Create a function to write README file
@@ -15,10 +27,10 @@ function init() {}
 // Function call to initialize app
 init();
 
-
+/*
 inquirer
-  .prompt([
-    {
+.prompt([
+  {
       type: 'input',
       message: 'What is your user name?',
       name: 'username',
@@ -37,3 +49,4 @@ inquirer
   .then((response) =>
     console.log(response.username, response.language, response.method)
   );
+*/
